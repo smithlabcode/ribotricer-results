013dc3999f55402fede911c4accbfc3e918afa80
Wenzheng Li <wenzhenl@usc.edu>
2018-11-19 13:46:10 -0800

    postpone filter later when merging results from different methods, so that the filter is common for each method

--- scripts/generate_ROC_input.py
+++ scripts/generate_ROC_input.py
@@ -8,49 +8,88 @@ import sys
 
 cmd, ref, pro = sys.argv
 
+
 # read result
 def read_resultccds(filename):
-	df = pd.read_table(filename,index_col=0,na_values='NA')
-	return df
-def read_ribocode(filename):
-	df = pd.read_table(filename,index_col=0,na_values='NAN')
-	return df
-def read_ribocop(filename):
-	df = pd.read_table(filename,index_col=0,na_values='NAN')
-	return df
+    df = pd.read_table(filename, index_col=0, na_values='NA')
+    return df
 
-def roc_output(df,outname="ROC_input.txt"):
-	with open(outname,"w") as fout:
-		fout.write("\t".join(["name","ribotaper","chisq","ORFscore","ribocode","ribocop","truth"]) + "\n")
-		for j in df.index:
-			if np.nan not in df.ix[j][["pval_multit_3nt_ribo","chisq_ribo","ORF_score_ribo","code_ribo","ribo_coh"]]:
-				fout.write(j + "_ribo" + "\t" + "\t".join(map(str,df.ix[j][["pval_multit_3nt_ribo","chisq_ribo","ORF_score_ribo","code_ribo","ribo_coh"]])) + "\t1\t\n")
-			if np.nan not in df.ix[j][["pval_multit_3nt_rna","chisq_rna","ORF_score_rna","code_rna","rna_coh"]]:
-				fout.write(j + "_rna" + "\t" + "\t".join(map(str,df.ix[j][["pval_multit_3nt_rna","chisq_rna",
-				                                                                  "ORF_score_rna","code_rna","rna_coh"]])) + "\t0\t\n")
+
+def read_ribocode(filename):
+    df = pd.read_table(filename, index_col=0, na_values='NAN')
+    return df
+
+
+def read_ribocop(filename):
+    df = pd.read_table(filename, index_col=0, na_values='NAN')
+    return df
+
+
+def roc_output(df, outname="ROC_input.txt"):
+    with open(outname, "w") as fout:
+        fout.write("\t".join([
+            "name", "ribotaper", "chisq", "ORFscore", "ribocode", "ribocop",
+            "truth"
+        ]) + "\n")
+        for j in df.index:
+            if np.nan not in df.ix[j][[
+                    "pval_multit_3nt_ribo", "chisq_ribo", "ORF_score_ribo",
+                    "code_ribo", "ribo_coh"
+            ]]:
+                fout.write(j + "_ribo" + "\t" + "\t".join(
+                    map(
+                        str, df.ix[j][[
+                            "pval_multit_3nt_ribo", "chisq_ribo",
+                            "ORF_score_ribo", "code_ribo", "ribo_coh"
+                        ]])) + "\t1\t\n")
+            if np.nan not in df.ix[j][[
+                    "pval_multit_3nt_rna", "chisq_rna", "ORF_score_rna",
+                    "code_rna", "rna_coh"
+            ]]:
+                fout.write(j + "_rna" + "\t" + "\t".join(
+                    map(
+                        str, df.ix[j][[
+                            "pval_multit_3nt_rna", "chisq_rna",
+                            "ORF_score_rna", "code_rna", "rna_coh"
+                        ]])) + "\t0\t\n")
 
 
 #main
 #1. read result
 # ribotaper_df = read_resultccds("results_ccds")
-ribotaper_result = '/home/cmb-panasas2/wenzhenl/benchmark/{}/{}/results_ccds'.format(ref, pro)
-ribotaper_df = read_resultccds(ribotaper_result)
+ribotaper_result = '/home/cmb-panasas2/wenzhenl/benchmark/{}/{}/results_ccds'.format(
+    ref, pro)
+ribotaper = read_resultccds(ribotaper_result)
 # ribocode = read_ribocode("ribocode_result.txt")
-ribocode_result = '/home/cmb-panasas2/wenzhenl/benchmark/{}/{}/ribocode_result.txt'.format(ref, pro)
+ribocode_result = '/home/cmb-panasas2/wenzhenl/benchmark/{}/{}/ribocode_result.txt'.format(
+    ref, pro)
 ribocode = read_ribocode(ribocode_result)
 # ribocop = read_ribocop("ribocop_result.txt")
-ribocop_result = '/home/cmb-panasas2/wenzhenl/benchmark/{}/{}/ribocop_results.txt'.format(ref, pro)
+ribocop_result = '/home/cmb-panasas2/wenzhenl/benchmark/{}/{}/ribocop_results.txt'.format(
+    ref, pro)
 ribocop = read_ribocop(ribocop_result)
 
+##Fill NA
 ##Drop NA values
 ##Not sure if this is fair at all
-ribotaper_df_na = ribotaper_df[(ribotaper_df["pval_multit_3nt_ribo"] != np.nan) | (ribotaper_df["pval_multit_3nt_rna"] != np.nan)]
-ribocode_df_na = ribocode[(ribocode["code_ribo"] != np.nan) | (ribocode['code_rna'] != np.nan)]
-ribocop_df_na = ribocop[(ribocop["ribo_coh"] != np.nan) | (ribocop['rna_coh'] != np.nan)]
+# ribotaper_df_na = ribotaper_df[(ribotaper_df["pval_multit_3nt_ribo"] != np.nan) | (ribotaper_df["pval_multit_3nt_rna"] != np.nan)]
+# ribocode_df_na = ribocode[(ribocode["code_ribo"] != np.nan) | (ribocode['code_rna'] != np.nan)]
+# ribocop_df_na = ribocop[(ribocop["ribo_coh"] != np.nan) | (ribocop['rna_coh'] != np.nan)]
+ribotaper[['pval_multit_3nt_ribo', 'pval_multit_3nt_rna']] = ribotaper[[
+    'pval_multit_3nt_ribo', 'pval_multit_3nt_rna'
+]].fillna(1.0)
+ribotaper[['ORF_score_ribo',
+           'ORF_score_rna']] = ribotaper[['ORF_score_ribo',
+                                          'ORF_score_rna']].fillna(0.0)
+ribocode[['code_ribo', 'code_rna']] = ribocode[['code_ribo',
+                                                'code_rna']].fillna(1.0)
+ribocop[['ribo_coh', 'rna_coh']] = ribocop[['ribo_coh', 'rna_coh']].fillna(0.0)
 
 #2. merger
-merger_df = ribotaper_df_na.merge(ribocode_df_na,how="inner",left_index=True,right_index=True)
-merger_df = merger_df.merge(ribocop_df_na,how="inner",left_index=True,right_index=True)
+merger_df = ribotaper.merge(
+    ribocode, how="inner", left_index=True, right_index=True)
+merger_df = merger_df.merge(
+    ribocop, how="inner", left_index=True, right_index=True)
 
 #3. filter the psites_sum < 5:
 merger_df_filter = merger_df[merger_df.P_sites_sum > 5]
--- scripts/run_ribotaper.sh
+++ scripts/run_ribotaper.sh
@@ -7,6 +7,7 @@ reads=$5
 offsets=$6
 mkdir -p /home/cmb-panasas2/wenzhenl/benchmark/${ref}/${pro}
 cd /home/cmb-panasas2/wenzhenl/benchmark/${ref}/${pro}
-echo "ribo: ${ribo}\nrna: ${rna}\n" > samples_used.txt
-echo "read lengths: ${reads}\noffsets: ${offsets}\n" >> samples_used.txt
+printf "ribo: ${ribo}\nrna: ${rna}\n" > samples_used.txt
+printf "read lengths: ${reads}\noffsets: ${offsets}\n" >> samples_used.txt
+printf "/home/cmb-panasas2/wenzhenl/github/ribotaper/scripts/Ribotaper.sh /staging/as/wenzhenl/re-ribo-analysis/${pro}/mapped/bams/${ribo}.bam /staging/as/wenzhenl/re-ribo-analysis/${pro}/mapped/bams/${rna}.bam /home/cmb-panasas2/wenzhenl/genomes/${ref}/annotation/ribotaper_annotation/ $reads $offsets /home/cmb-panasas2/wenzhenl/github/ribotaper/scripts/ /home/cmb-panasas2/wenzhenl/miniconda3/envs/ribotaper/bin/ 16" >> samples_used.txt
 /home/cmb-panasas2/wenzhenl/github/ribotaper/scripts/Ribotaper.sh /staging/as/wenzhenl/re-ribo-analysis/${pro}/mapped/bams/${ribo}.bam /staging/as/wenzhenl/re-ribo-analysis/${pro}/mapped/bams/${rna}.bam /home/cmb-panasas2/wenzhenl/genomes/${ref}/annotation/ribotaper_annotation/ $reads $offsets /home/cmb-panasas2/wenzhenl/github/ribotaper/scripts/ /home/cmb-panasas2/wenzhenl/miniconda3/envs/ribotaper/bin/ 16
