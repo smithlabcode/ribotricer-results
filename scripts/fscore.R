59ee44e46194c014cbdca55e9dcd00d3b5066732
Wenzheng Li <wenzhenl@usc.edu>
2018-11-20 11:40:42 -0800

    bug fixed, fscore is corrected

--- scripts/fscore.R
+++ scripts/fscore.R
@@ -1,13 +1,17 @@
 options(echo = TRUE)
 args = commandArgs(trailingOnly = TRUE)
 # defualt 0.05
-ribotaper_cutoff = args[1]
+# ribotaper_cutoff = args[1]
+ribotaper_cutoff = 0.05
 # default 6.044
-ORFscore_cutoff = args[2]
+# ORFscore_cutoff = args[2]
+ORFscore_cutoff = 6.044
 # default 0.05
-ribocode_cutoff = args[3]
+# ribocode_cutoff = args[3]
+ribocode_cutoff = 0.05
 #default 0.4
-ribocop_cutoff = args[4]
+ribocop_cutoff = as.double(args[1])
+# ribocop_cutoff = 0.40528
 
 # ground truth
 data <- read.table("ROC_input.txt",header=TRUE,row.names=1)
@@ -20,9 +24,9 @@ true_positives = intersect(positives, ribotaper_positives)
 true_negatives = intersect(negatives, ribotaper_negatives)
 sensitivity = length(true_positives) / length(positives)
 specificity = length(true_negatives) / length(negatives)
-fscore = 2 * sensitivity * specificity / (sensitivity + specificity)
 precision = length(true_positives) / length(ribotaper_positives)
 recall = length(true_positives) / length(positives)
+fscore = 2 * sensitivity * precision / (sensitivity + precision)
 res.ribotaper = data.frame(ribotaper=c(sensitivity, specificity, fscore,
                                        precision, recall))
 # ORFscore results
@@ -32,9 +36,9 @@ true_positives = intersect(positives, ORFscore_positives)
 true_negatives = intersect(negatives, ORFscore_negatives)
 sensitivity = length(true_positives) / length(positives)
 specificity = length(true_negatives) / length(negatives)
-fscore = 2 * sensitivity * specificity / (sensitivity + specificity)
 precision = length(true_positives) / length(ORFscore_positives)
 recall = length(true_positives) / length(positives)
+fscore = 2 * sensitivity * precision / (sensitivity + precision)
 res.ORFscore = data.frame(ORFscore=c(sensitivity, specificity, fscore,
                                        precision, recall))
 # ribocode results
@@ -44,9 +48,9 @@ true_positives = intersect(positives, ribocode_positives)
 true_negatives = intersect(negatives, ribocode_negatives)
 sensitivity = length(true_positives) / length(positives)
 specificity = length(true_negatives) / length(negatives)
-fscore = 2 * sensitivity * specificity / (sensitivity + specificity)
 precision = length(true_positives) / length(ribocode_positives)
 recall = length(true_positives) / length(positives)
+fscore = 2 * sensitivity * precision / (sensitivity + precision)
 res.ribocode = data.frame(ribocode=c(sensitivity, specificity, fscore,
                                        precision, recall))
 # ribocop results
@@ -56,9 +60,9 @@ true_positives = intersect(positives, ribocop_positives)
 true_negatives = intersect(negatives, ribocop_negatives)
 sensitivity = length(true_positives) / length(positives)
 specificity = length(true_negatives) / length(negatives)
-fscore = 2 * sensitivity * specificity / (sensitivity + specificity)
 precision = length(true_positives) / length(ribocop_positives)
 recall = length(true_positives) / length(positives)
+fscore = 2 * sensitivity * precision / (sensitivity + precision)
 res.ribocop = data.frame(ribocop=c(sensitivity, specificity, fscore,
                                        precision, recall))
 res = cbind(res.ribotaper, res.ORFscore, res.ribocode, res.ribocop)
