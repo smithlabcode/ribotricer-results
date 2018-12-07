978a1952f0915b5bd446f70c75d3e060327fb39c
Wenzheng Li <wenzhenl@usc.edu>
2018-12-06 16:45:46 -0800

    automate the process

--- scripts/ccds_bam.sh
+++ scripts/ccds_bam.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
-ccds=$1
-inbam=$2
-outbam=$3
-bedtools intersect -abam inbam -b ccds -s > outbam
+ref=$1
+pro=$2
+lengths=$3
+dst="/home/cmb-panasas2/wenzhenl/benchmark/gene_level/${ref}/${pro}"
+mkdir -p $dst
+cd $dst
+ccds="/home/cmb-panasas2/wenzhenl/benchmark/gene_level/${ref}_frame_ccds.bed"
+ribobam="/home/cmb-panasas2/wenzhenl/benchmark/exon_level/${ref}/${pro}/RIBO_best.bam"
+rnabam="/home/cmb-panasas2/wenzhenl/benchmark/exon_level/${ref}/${pro}/RNA_best.bam"
+printf "intersecting Ribo\n"
+bedtools intersect -abam $ribobam -b $ccds -s > RIBO_CCDS.bam
+printf "intersecting RNA\n"
+bedtools intersect -abam $rnabam -b $ccds -s > RNA_CCDS.bam
+printf "filtering Ribo for read lengths\n"
+python /home/cmb-panasas2/wenzhenl/benchmark/scripts/bam_of_lengths.py RIBO_CCDS.bam RIBO_CCDS_lengths.bam $lengths
