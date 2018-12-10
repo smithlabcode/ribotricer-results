98dd816bf5100713fe9c69f65803409066b0bfd8
Wenzheng Li <wenzhenl@usc.edu>
2018-12-09 22:09:27 -0800

    added conda env info for ORF-RATER

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/bash
-ref=$1
-pro=$2
-cd /home/cmb-panasas2/wenzhenl/benchmark/${ref}/${pro}
-echo 'running RiboCode'
-source activate roc27
-python /home/cmb-panasas2/wenzhenl/benchmark/scripts/generate_ribocode_pvalue.py $ref $pro
-echo 'running RiboCop'
 source activate riboraptor
-RiboCop benchmark --rna data_tracks/Centered_RNA_tracks_ccds --ribo data_tracks/P_sites_all_tracks_ccds --prefix ribocop
-echo 'running ROC'
-python /home/cmb-panasas2/wenzhenl/benchmark/scripts/generate_ROC_input.py $ref $pro
-Rscript /home/cmb-panasas2/wenzhenl/benchmark/scripts/ROC.R
-mkdir -p /staging/as/wenzhenl/jupyter/roc/${ref}/${pro}
-cp auc.txt /staging/as/wenzhenl/jupyter/roc/${ref}/${pro}
-cp ROC.pdf /staging/as/wenzhenl/jupyter/roc/${ref}/${pro}
-echo 'running theta'
-RiboCop theta-dist --rna data_tracks/Centered_RNA_tracks_ccds --ribo data_tracks/P_sites_all_tracks_ccds --frame /home/cmb-panasas2/wenzhenl/genomes/${ref}/annotation/ribotaper_annotation/frames_ccds --prefix ${pro}
-Rscript /home/cmb-panasas2/wenzhenl/benchmark/scripts/theta.R ${pro}
-mkdir -p /staging/as/wenzhenl/jupyter/theta/${ref}/${pro}
-cp ${pro}_angle_stats.txt /staging/as/wenzhenl/jupyter/theta/${ref}/${pro}
-cp ${pro}_theta_dist.pdf /staging/as/wenzhenl/jupyter/theta/${ref}/${pro}
+file=$1
+while read line
+do
+    read -r -a array <<< $line
+    pro=${array[0]}
+    ref=${array[1]}
+    bash run_star.sh $ref $pro
+done < $file
