d696768deb2cfafe2b5487d43f56e3ed2cd772a1
Saket Choudhary <saketkc@gmail.com>
2018-12-10 22:40:24 -0800

    add riboHMM pipeline

--- tools/riboHMM/03.mappability_hg38.sh
+++ tools/riboHMM/03.mappability_hg38.sh
@@ -7,21 +7,21 @@ export IN_FQ="/staging/as/skchoudh/riboHMM_out/hg38_synfootprints_29.fq.gz"
 export STAR_INDEX="/home/cmb-panasas2/skchoudh/genomes/hg38/star_annotated"
 export OUT_MAPPABILITY="/staging/as/skchoudh/riboHMM_out/hg38_synfootprints_mappability_29"
 
-STAR --runThreadN 16\
-  --genomeDir ${STAR_INDEX}\
-  --outFilterMismatchNmax 2\
-  --outFileNamePrefix ${OUT_PREFIX}\
-  --readFilesIn ${IN_FQ}\
-  --readFilesCommand zcat\
-  --quantMode TranscriptomeSAM\
-  --outSAMtype BAM Unsorted\
-  --outFilterType BySJout\
-  --outFilterMatchNmin 16\
-  --seedSearchStartLmax 15\
-  --winAnchorMultimapNmax 200\
-  && samtools sort -@ 16 ${OUT_PREFIX}Aligned.out.bam -o ${OUT_BAM}\ 
-  && samtools index ${OUT_BAM}\
-  && mv ${OUT_PREFIX}Log.final.out ${OUT_PREFIX}Log.out ${OUT_PREFIX}SJ.out.tab
+##STAR --runThreadN 16\
+##  --genomeDir ${STAR_INDEX}\
+##  --outFilterMismatchNmax 2\
+##  --outFileNamePrefix ${OUT_PREFIX}\
+##  --readFilesIn ${IN_FQ}\
+##  --readFilesCommand zcat\
+##  --quantMode TranscriptomeSAM\
+##  --outSAMtype BAM Unsorted\
+##  --outFilterType BySJout\
+##  --outFilterMatchNmin 16\
+##  --seedSearchStartLmax 15\
+##  --winAnchorMultimapNmax 200
+##samtools sort -@ 16 ${OUT_PREFIX}Aligned.out.bam -o ${OUT_BAM}
+##samtools index ${OUT_BAM}
+##mv ${OUT_PREFIX}Log.final.out ${OUT_PREFIX}Log.out ${OUT_PREFIX}SJ.out.tab
 
 #samtools sort -@ 16 ${OUT_PREFIX}Aligned.out.bam -o ${OUT_BAM}
 #samtools index ${OUT_BAM}
---
+++ tools/riboHMM/configs/SRP029589_human.py
@@ -0,0 +1,9 @@
+
+INPUT_DIR = '/home/cmb-panasas2/skchoudh/benchmark/gene_level/hg38/SRP029589_human'
+SYNFOOTPRINTS_PREFIX = '/staging/as/skchoudh/riboHMM_synthetic_footprints/hg38_synfootprints'
+READ_LENGTHS = ['29', '30', '32']
+INTRON_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.intron.bed'
+CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/hg38/fasta/hg38.chrom.sizes'
+GTF = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.annotation.gtf'
+GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/hg38/fasta/hg38.fa'
+STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/hg38/star_annotated/'
