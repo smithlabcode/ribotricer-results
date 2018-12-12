3c4cd84e8471dad635f53bb691fdd91adbc25417
Wenzheng Li <wenzhenl@usc.edu>
2018-12-12 15:38:35 -0800

    removed unused scripts

--- tools/rpbp/run_RpBp.sh
+++
@@ -1,2 +0,0 @@
-prepare-rpbp-genome prepare.yaml --num-cpus 8 --mem 40G --overwrite --logging-level INFO
-run-all-rpbp-instances config.yaml --overwrite --num-cpus 8 --logging-level INFO --log-file log.alignments-only.txt --keep-intermediate-files
