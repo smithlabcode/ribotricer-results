e9a3bcc2497903254c3556126cdc7b8b0435dd10
Wenzheng Li <wenzhenl@usc.edu>
2018-12-16 23:00:17 -0800

    generate true positive CCDS genes with more information. Especially now the cooridinate of each exon matches what in GTF

--- scripts/gene_scripts/tp_ccds.py
+++ scripts/gene_scripts/tp_ccds.py
@@ -13,14 +13,27 @@ all 0-based half open.
 """
 import sys
 from tqdm import *
+from collections import namedtuple
+ORF = namedtuple('ORF', 'oid, otype, tid, ttype, gid, gname, gtype, chrom, strand')
 
 ccds_file = sys.argv[1]
 ribo_file = sys.argv[2]
 rna_file = sys.argv[3]
-prefix = sys.argv[4]
+annot_file = sys.argv[4]
+prefix = sys.argv[5]
 cutoff = 5 # number of reads in exon to be considered as translating
 min_len = 10 # minimum length of CCDS to be included
 
+cop_annot = {}
+with open(annot_file, 'r') as fin:
+    header=True
+    for r in fin:
+        if header:
+            header=False
+            continue
+        oid, otype, tid, ttype,gid, gname, gtype, chrom, strand, coordinate, _, _, _ = r.split('\t') 
+        orf = ORF._make([oid, otype, tid, ttype, gid, gname, gtype, chrom, strand])
+        cop_annot[gid, coordinate] = orf
 rna = {}
 ribo = {}
 print('reading RNA profiles')
@@ -51,8 +64,9 @@ with open(ribo_file, 'r') as orf:
             end = int(end)
             ribo[(chrom, start, end, strand)] = [total, gid]
 
-to_write = ''
+to_write = 'ORF_ID\tgene_id\ttranscript_id\tchrom\tstrand\tcooridinate\n'
 keyerror = 0
+missinginannot = 0
 total_exons = 0
 print('reading CCDS annotation')
 with open(ccds_file, 'r') as orf:
@@ -75,6 +89,7 @@ with open(ccds_file, 'r') as orf:
             rna_reads = 0
             gene_ids = set()
             orf_length = 0
+            cop_id = []
             for i, loc in enumerate(locations):
                 total_exons += 1
                 if strand == '+' and i == len(locations)-1:
@@ -86,6 +101,7 @@ with open(ccds_file, 'r') as orf:
                 else:
                     start = loc[0]
                     end = loc[1] + 1
+                cop_id.append('{}-{}'.format(start+1, end))
                 orf_length += end - start
                 try:
                     nread, gid = rna[(chrom, start, end, strand)]
@@ -101,10 +117,19 @@ with open(ccds_file, 'r') as orf:
                     continue
             if (valid and rna_reads >= cutoff and len(gene_ids) == 1 and
                     orf_length >= min_len):
-                to_write += '{}\n'.format(list(gene_ids)[0])
-print('Total exons: {}\nKey Errors: {}\n'.format(total_exons, keyerror))
-with open('{}_TP_genes.txt'.format(prefix), 'w') as output:
+                cop_id = ','.join(cop_id)
+                geneid = list(gene_ids)[0]
+                if (geneid, cop_id) in cop_annot:
+                    orf = cop_annot[geneid, cop_id]
+                    # assert(orf.gid == list(gene_ids)[0])
+                    # if orf.gid != list(gene_ids)[0]:
+                    #     print(orf.gid, list(gene_ids)[0])
+                    #     print(cop_id)
+                    #     break
+                    to_write += '{}\t{}\t{}\t{}\t{}\t{}\n'.format(orf.oid, orf.gid,
+                            orf.tid, orf.chrom, orf.strand, cop_id)
+                else:
+                    missinginannot += 1
+print('Total exons: {}\nKey Errors: {}\nmissing: {}\n'.format(total_exons, keyerror, missinginannot))
+with open('{}_TP_CCDS_genes.txt'.format(prefix), 'w') as output:
     output.write(to_write)
-
-
-
