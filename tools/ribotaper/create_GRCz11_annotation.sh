4770a0ee96cc21f6a456d6a90e79c4e0d0ec866e
Saket Choudhary <saketkc@gmail.com>
2019-09-07 21:13:33 -0700

    bootstrap notebook

--- tools/ribotaper/create_GRCz11_annotation.sh
+++ tools/ribotaper/create_GRCz11_annotation.sh
@@ -1,1 +1,1 @@
-bash create_annotations_files.bash /home/cmb-panasas2/skchoudh/genomes/GRCz11/annotation/Danio_rerio.GRCz11.96.chr.gtf /home/cmb-panasas2/skchoudh/genomes/GRCz11/fasta/Danio_rerio.GRCz11.dna.primary_assembly.fa false false /home/cmb-panasas2/skchoudh/genomes/GRCz11/ribotaper_annotation_v96
+bash create_annotations_files.bash /home/cmb-panasas2/skchoudh/genomes/GRCz11/annotation/Danio_rerio.GRCz11.96.gtf /home/cmb-panasas2/skchoudh/genomes/GRCz11/fasta/Danio_rerio.GRCz11.dna.primary_assembly.fa false false /home/cmb-panasas2/skchoudh/genomes/GRCz11/ribotaper_annotation_v96
